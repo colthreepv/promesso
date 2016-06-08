e5ee649059f88ddfc996192c546138d31a34a20b
colthreepv <mister.gamer@gmail.com>
2016-06-09 01:31:29 +0200

    new 4.x version handling arrays and raw functions
    needs more testing and documentation, that's why it's in beta

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const validation = require('express-validation');
 const XError = require('x-error');
+const ValidationError = validation.ValidationError;
 const Promise = require('bluebird');
 
 const logger = {
@@ -14,28 +15,47 @@ function changeLogger (loggerFn, errorFn) {
   if (typeof errorFn === 'function') logger.error = errorFn;
 }
 
-// TODO: handle Array middlewares
+/**
+ * promesso takes a Promise-based middleware function and converts it to a classic array of middlewares
+ *
+ * @param  {Function|Function[]} handler: Promise-based Express middleware
+ * @return {Function[]} Express-compliant Array of middlewares
+ */
 function promesso (handler) {
+  const handleFn = isFunction(handler) ? [handler] : handler;
   const middlewares = [];
-  const befores = handler['@before'];
-  if (isObject(befores)) befores.forEach(m => middlewares.push(middleFactory(m)));
-  if (isObject(handler['@validation'])) middlewares.push(validation(handler['@validation']));
+  let validations = 0;
 
-  if (handler['@type'] === 'raw') middlewares.push(handler);
-  else middlewares.push(handleFactory(handler));
+  handleFn.forEach(h => {
+    if (!isFunction(h)) throw new Error('Handler is expected to be a function or an Array of functions.');
 
-  return middlewares;
+    if (isObject(handleFn['@validation'])) {
+      middlewares.push(validation(h['@validation']), validationMiddleware);
+      if (++validations > 1) throw new Error('Every handler can have only one validator per chain.');
+    }
+
+    middlewares.push(h);
+  });
+
+  return middlewares.map(rawOrPromise);
 }
 promesso.logger = changeLogger;
 
-function handleFactory (handler) {
+function rawOrPromise (handler, index, array) {
+  if (handler['@raw']) return handler;
+  else return handleFactory(handler, (index !== array.length - 1));
+}
+
+function handleFactory (handler, usesNext) {
 
   // handle more cases
-  return function (req, res) {
+  return function (req, res, next) {
     const promisified = Promise.method(handler);
     return promisified(req) // express ignores return value, but useful for testing
     .then(response => {
-      if (Array.isArray(response)) { // array structure
+      if (usesNext) return next();
+      // array structure
+      if (Array.isArray(response)) {
         response.forEach(r => res[r.method].apply(res, r.args));
       } else {
         res.status(200).send(response);
@@ -47,22 +67,8 @@ function handleFactory (handler) {
   };
 }
 
-function middleFactory (handler) {
-  return function (req, res, next) {
-    const promisified = Promise.method(handler);
-    promisified(req)
-      .then(() => {
-        next();
-      })
-      .catch(XError, xerrorHandler.bind(null, res))
-      .catch(Error, errorHandler.bind(null, req, res))
-      .catch(genericHandler.bind(null, res));
-  };
-}
-
-function isObject (obj) {
-  return (obj !== null && typeof obj === 'object');
-}
+function isObject (obj) { return (obj !== null && typeof obj === 'object'); }
+function isFunction (fn) { return typeof fn === 'function'; }
 
 /**
  * Error Handlers
@@ -72,8 +78,9 @@ function xerrorHandler (res, err) {
 
   const httpCode = err.httpCode || 500;
   const httpResponse = { code: err.code };
+  if (typeof err.httpResponse === 'object') return res.status(httpCode).send(err.httpResponse);
   if (typeof err.httpResponse === 'string') httpResponse.message = err.httpResponse;
-  return res.status(httpCode).send(err.httpResponse || httpResponse);
+  return res.status(httpCode).send(httpResponse);
 }
 function errorHandler (req, res, err) {
   logger.log(err.stack, 'coding error', { body: req.body, query: req.query, params: req.params, ip: req.ip, status: 500 });
@@ -85,5 +92,14 @@ function genericHandler (res, err) {
   logger.error(err);
   return res.sendStatus(500);
 }
+
+/**
+ * Handles thrown errors from express-validation
+ */
+function validationMiddleware (err, req, res, next) {
+  if (!err instanceof ValidationError) return next(err);
+  return res.status(err.status).send({ errors: err.errors });
+}
+validationMiddleware['@raw'] = true;
 
 module.exports = promesso;
